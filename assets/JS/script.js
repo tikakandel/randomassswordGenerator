48ba2f0259f13d0cf083226f3aebddec85e2fbd0
tikak <tika.kandel.06@gmail.com>
2021-06-09 11:10:58 +1000

    aded the check list

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,7 +2,14 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+var capitalValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var smallValues = "abcdefghijklmnopqrstwxyz";
+var numberValues = "1234567890";
+var specialValues = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+let values =""
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
